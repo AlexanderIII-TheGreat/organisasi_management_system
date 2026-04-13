2d9b4c03e608a3b14be7e2867dc73540fb8a8c35
AlexanderIII-TheGreat <abiyyu040@gmail.com>
2026-04-13 15:56:09 +0700

    commits

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_IMAGE_DOMAIN || 'localhost',
+        pathname: '/storage/**',
+      },
+      // Juga izinkan http untuk development lokal
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        pathname: '/storage/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
